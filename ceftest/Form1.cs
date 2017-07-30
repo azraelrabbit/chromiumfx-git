e67ba01ad80a6af639d7d8cac3f95884604e3d1b
rock_82 <rock_82@163.com>
2017-07-30 18:28:48 +0800

    add missing files

---
+++ ceftest/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+ 
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Chromium;
+using Chromium.WebBrowser;
+
+namespace ceftest
+{
+    public partial class Form1 : Form
+    {
+        private Chromium.WebBrowser.ChromiumWebBrowser webBrowser;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webBrowser=new ChromiumWebBrowser();
+           webBrowser.Dock=DockStyle.Fill;
+           
+            
+            panel1.Controls.Add(webBrowser);
+            panel1.Refresh();
+            //CfxRuntime.DoMessageLoopWork();
+        }
+ 
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            var url = textBox1.Text;
+
+            webBrowser.LoadUrl(url);
+            webBrowser.Refresh();
+        }
+    }
+}
