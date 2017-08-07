55e5ac21e0c8f04def381e319fada7f914379bb6
azraelrabbit <azraelrabbit@localhost>
2017-08-07 21:26:15 +0800

    fixed windowless on windows keypress and keydown issue.

--- Tests/Windowless/BrowserControl.cs
+++ Tests/Windowless/BrowserControl.cs
@@ -9,12 +9,14 @@ using System.Drawing;
 using System.Windows.Forms;
 using Chromium;
 
-namespace Windowless {
+namespace Windowless
+{
     /// <summary>
     /// A minimum and very incomplete implementation of a
     /// control with windowless browser.
     /// </summary>
-    class BrowserControl : Control {
+    class BrowserControl : Control
+    {
 
         internal CfxBrowser browser;
 
@@ -23,19 +25,21 @@ namespace Windowless {
         private CfxLoadHandler loadHandler;
         private CfxRenderHandler renderHandler;
 
-		private CfxKeyboardHandler keyboardHandler;
+        private CfxKeyboardHandler keyboardHandler;
 
         private Bitmap pixelBuffer;
         private object pbLock = new object();
 
-        public BrowserControl(Control parent) {
-
-			this.Parent=parent;
+        private static bool _mono;
+        public BrowserControl(Control parent)
+        {
+            _mono = Type.GetType("Mono.Runtime") != null;
+            this.Parent = parent;
             SetStyle(ControlStyles.AllPaintingInWmPaint, true);
 
-			Console.WriteLine ("can enable ime:"+this.CanEnableIme);
+            Console.WriteLine("can enable ime:" + this.CanEnableIme);
 
-			this.ImeMode = ImeMode.On;
+            this.ImeMode = ImeMode.On;
 
             lifeSpanHandler = new CfxLifeSpanHandler();
             lifeSpanHandler.OnAfterCreated += lifeSpanHandler_OnAfterCreated;
@@ -48,7 +52,7 @@ namespace Windowless {
             renderHandler.GetViewRect += renderHandler_GetViewRect;
             renderHandler.OnCursorChange += renderHandler_OnCursorChange;
             renderHandler.OnPaint += renderHandler_OnPaint;
-		
+
             //renderHandler.OnPopupShow += renderHandler_OnPopupShow;
             //renderHandler.OnPopupSize += renderHandler_OnPopupSize;
             //renderHandler.OnScrollOffsetChanged += renderHandler_OnScrollOffsetChanged;
@@ -60,88 +64,113 @@ namespace Windowless {
             loadHandler.OnLoadError += loadHandler_OnLoadError;
 
 
-			keyboardHandler = new CfxKeyboardHandler ();
+            keyboardHandler = new CfxKeyboardHandler();
 
 
-			keyboardHandler.OnPreKeyEvent+= KeyboardHandler_OnPreKeyEvent;
+            keyboardHandler.OnPreKeyEvent += KeyboardHandler_OnPreKeyEvent;
 
             client = new CfxClient();
             client.GetLifeSpanHandler += (sender, e) => e.SetReturnValue(lifeSpanHandler);
             client.GetRenderHandler += (sender, e) => e.SetReturnValue(renderHandler);
             client.GetLoadHandler += (sender, e) => e.SetReturnValue(loadHandler);
-			client.GetKeyboardHandler += (sender, e) => e.SetReturnValue (keyboardHandler);
+            client.GetKeyboardHandler += (sender, e) => e.SetReturnValue(keyboardHandler);
 
             var settings = new CfxBrowserSettings();
 
             var windowInfo = new CfxWindowInfo();
 
-			windowInfo.SetAsWindowless (this.Handle);
+            windowInfo.SetAsWindowless(this.Handle);
             //windowInfo.SetAsWindowless(this.Parent.Handle);
 
             // Create handle now for InvokeRequired to work properly 
-           // CreateHandle();
+            // CreateHandle();
             CfxBrowserHost.CreateBrowser(windowInfo, client, "about:blank", settings, null);
 
         }
 
-        void KeyboardHandler_OnPreKeyEvent (object sender, Chromium.Event.CfxOnPreKeyEventEventArgs e)
+        protected override void Dispose(bool disposing)
         {
-		//	Console.WriteLine ((char)e.Event.UnmodifiedCharacter);
+            //this.browser.Dispose();
+            //this.client.Dispose();
+
+
+            base.Dispose(disposing);
+        }
+
+        void KeyboardHandler_OnPreKeyEvent(object sender, Chromium.Event.CfxOnPreKeyEventEventArgs e)
+        {
+            //	Console.WriteLine ((char)e.Event.UnmodifiedCharacter);
 
 
         }
 
-        void loadHandler_OnLoadError(object sender, Chromium.Event.CfxOnLoadErrorEventArgs e) {
-            if(e.ErrorCode == CfxErrorCode.Aborted) {
+        void loadHandler_OnLoadError(object sender, Chromium.Event.CfxOnLoadErrorEventArgs e)
+        {
+            if (e.ErrorCode == CfxErrorCode.Aborted)
+            {
                 // this seems to happen when calling LoadUrl and the browser is not yet ready
                 var url = e.FailedUrl;
                 var frame = e.Frame;
-                System.Threading.ThreadPool.QueueUserWorkItem((state) => {
+                System.Threading.ThreadPool.QueueUserWorkItem((state) =>
+                {
                     System.Threading.Thread.Sleep(200);
                     frame.LoadUrl(url);
                 });
             }
         }
 
-        void renderHandler_UpdateDragCursor(object sender, Chromium.Event.CfxUpdateDragCursorEventArgs e) {
+        void renderHandler_UpdateDragCursor(object sender, Chromium.Event.CfxUpdateDragCursorEventArgs e)
+        {
             throw new NotImplementedException();
         }
 
-        void renderHandler_StartDragging(object sender, Chromium.Event.CfxStartDraggingEventArgs e) {
+        void renderHandler_StartDragging(object sender, Chromium.Event.CfxStartDraggingEventArgs e)
+        {
             throw new NotImplementedException();
         }
 
-        void renderHandler_OnScrollOffsetChanged(object sender, Chromium.Event.CfxOnScrollOffsetChangedEventArgs e) {
+        void renderHandler_OnScrollOffsetChanged(object sender, Chromium.Event.CfxOnScrollOffsetChangedEventArgs e)
+        {
             throw new NotImplementedException();
         }
 
-        void renderHandler_OnPopupSize(object sender, Chromium.Event.CfxOnPopupSizeEventArgs e) {
+        void renderHandler_OnPopupSize(object sender, Chromium.Event.CfxOnPopupSizeEventArgs e)
+        {
             throw new NotImplementedException();
         }
 
-        void renderHandler_OnPopupShow(object sender, Chromium.Event.CfxOnPopupShowEventArgs e) {
+        void renderHandler_OnPopupShow(object sender, Chromium.Event.CfxOnPopupShowEventArgs e)
+        {
             throw new NotImplementedException();
         }
 
-        void renderHandler_OnPaint(object sender, Chromium.Event.CfxOnPaintEventArgs e) {
+        void renderHandler_OnPaint(object sender, Chromium.Event.CfxOnPaintEventArgs e)
+        {
 
-            lock (pbLock) {
-                if(pixelBuffer == null || pixelBuffer.Width < e.Width || pixelBuffer.Height < e.Height) {
+            lock (pbLock)
+            {
+                if (pixelBuffer == null || pixelBuffer.Width < e.Width || pixelBuffer.Height < e.Height)
+                {
                     pixelBuffer = new Bitmap(e.Width, e.Height, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
                 }
-                using(var bm = new Bitmap(e.Width, e.Height, e.Width * 4, System.Drawing.Imaging.PixelFormat.Format32bppArgb, e.Buffer)) {
-                    using(var g = Graphics.FromImage(pixelBuffer)) {
+                using (var bm = new Bitmap(e.Width, e.Height, e.Width * 4, System.Drawing.Imaging.PixelFormat.Format32bppArgb, e.Buffer))
+                {
+                    using (var g = Graphics.FromImage(pixelBuffer))
+                    {
                         g.DrawImageUnscaled(bm, 0, 0);
                     }
                 }
             }
-            foreach(var r in e.DirtyRects) {
+            foreach (var r in e.DirtyRects)
+            {
                 Invalidate(new Rectangle(r.X, r.Y, r.Width, r.Height));
             }
         }
 
-        void renderHandler_OnCursorChange(object sender, Chromium.Event.CfxOnCursorChangeEventArgs e) {
-            switch(e.Type) {
+        void renderHandler_OnCursorChange(object sender, Chromium.Event.CfxOnCursorChangeEventArgs e)
+        {
+            switch (e.Type)
+            {
                 case CfxCursorType.Hand:
                     Cursor = Cursors.Hand;
                     break;
@@ -151,14 +180,17 @@ namespace Windowless {
             }
         }
 
-        void renderHandler_GetViewRect(object sender, Chromium.Event.CfxGetViewRectEventArgs e) {
+        void renderHandler_GetViewRect(object sender, Chromium.Event.CfxGetViewRectEventArgs e)
+        {
 
-            if(InvokeRequired) {
+            if (InvokeRequired)
+            {
                 Invoke((MethodInvoker)(() => renderHandler_GetViewRect(sender, e)));
                 return;
             }
 
-            if(!IsDisposed) {
+            if (!IsDisposed)
+            {
                 var origin = PointToScreen(new Point(0, 0));
                 e.Rect.X = origin.X;
                 e.Rect.Y = origin.Y;
@@ -168,14 +200,17 @@ namespace Windowless {
             }
         }
 
-        void renderHandler_GetScreenPoint(object sender, Chromium.Event.CfxGetScreenPointEventArgs e) {
+        void renderHandler_GetScreenPoint(object sender, Chromium.Event.CfxGetScreenPointEventArgs e)
+        {
 
-            if(InvokeRequired) {
+            if (InvokeRequired)
+            {
                 Invoke((MethodInvoker)(() => renderHandler_GetScreenPoint(sender, e)));
                 return;
             }
 
-            if(!IsDisposed) {
+            if (!IsDisposed)
+            {
                 var origin = PointToScreen(new Point(e.ViewX, e.ViewY));
                 e.ScreenX = origin.X;
                 e.ScreenY = origin.Y;
@@ -183,27 +218,30 @@ namespace Windowless {
             }
         }
 
-        void renderHandler_GetScreenInfo(object sender, Chromium.Event.CfxGetScreenInfoEventArgs e) {
+        void renderHandler_GetScreenInfo(object sender, Chromium.Event.CfxGetScreenInfoEventArgs e)
+        {
         }
 
-        void renderHandler_GetRootScreenRect(object sender, Chromium.Event.CfxGetRootScreenRectEventArgs e) {
+        void renderHandler_GetRootScreenRect(object sender, Chromium.Event.CfxGetRootScreenRectEventArgs e)
+        {
         }
 
 
-        void lifeSpanHandler_OnAfterCreated(object sender, Chromium.Event.CfxOnAfterCreatedEventArgs e) {
-          //  browser = e.Browser;
-          //  browser.MainFrame.LoadUrl("about:version");
-          //  if(Focused) {
-          //      browser.Host.SendFocusEvent(true);
-          //  }
-		   if (browser != null)
+        void lifeSpanHandler_OnAfterCreated(object sender, Chromium.Event.CfxOnAfterCreatedEventArgs e)
+        {
+            //  browser = e.Browser;
+            //  browser.MainFrame.LoadUrl("about:version");
+            //  if(Focused) {
+            //      browser.Host.SendFocusEvent(true);
+            //  }
+            if (browser != null)
             {
-               
+
             }
             else
             {
                 browser = e.Browser;
-				browser.MainFrame.LoadUrl("about:version");
+                browser.MainFrame.LoadUrl("about:version");
             }
 
             var br = e.Browser;
@@ -214,27 +252,34 @@ namespace Windowless {
             }
         }
 
-        protected override void OnGotFocus(EventArgs e) {
-            if(browser != null) {
+        protected override void OnGotFocus(EventArgs e)
+        {
+            if (browser != null)
+            {
                 browser.Host.SendFocusEvent(true);
             }
         }
 
         // control overrides
 
-        protected override void OnResize(EventArgs e) {
-            if(browser != null) {
+        protected override void OnResize(EventArgs e)
+        {
+            if (browser != null)
+            {
                 browser.Host.WasResized();
             }
         }
 
-        protected override void OnPaintBackground(PaintEventArgs pevent) {
+        protected override void OnPaintBackground(PaintEventArgs pevent)
+        {
             // do nothing
         }
 
-        protected override void OnPaint(PaintEventArgs e) {
-            lock (pbLock) {
-                if(pixelBuffer != null)
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            lock (pbLock)
+            {
+                if (pixelBuffer != null)
                     e.Graphics.DrawImage(pixelBuffer, e.ClipRectangle, e.ClipRectangle, GraphicsUnit.Pixel);
             }
         }
@@ -244,29 +289,37 @@ namespace Windowless {
 
         private readonly CfxMouseEvent mouseEventProxy = new CfxMouseEvent();
 
-        private void SetMouseEvent(MouseEventArgs e) {
+        private void SetMouseEvent(MouseEventArgs e)
+        {
             mouseEventProxy.X = e.X;
             mouseEventProxy.Y = e.Y;
         }
 
-        protected override void OnMouseMove(MouseEventArgs e) {
-            if(browser != null) {
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            if (browser != null)
+            {
                 SetMouseEvent(e);
                 browser.Host.SendMouseMoveEvent(mouseEventProxy, false);
             }
         }
 
-        protected override void OnMouseLeave(EventArgs e) {
-            if(browser != null) {
+        protected override void OnMouseLeave(EventArgs e)
+        {
+            if (browser != null)
+            {
                 browser.Host.SendMouseMoveEvent(mouseEventProxy, true);
             }
         }
 
-        protected override void OnMouseDown(MouseEventArgs e) {
-            if(browser != null) {
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            if (browser != null)
+            {
                 SetMouseEvent(e);
                 CfxMouseButtonType t;
-                switch(e.Button) {
+                switch (e.Button)
+                {
                     case System.Windows.Forms.MouseButtons.Right:
                         t = CfxMouseButtonType.Left;
                         break;
@@ -281,11 +334,14 @@ namespace Windowless {
                 browser.Host.SendMouseClickEvent(mouseEventProxy, t, false, e.Clicks);
             }
         }
-        protected override void OnMouseUp(MouseEventArgs e) {
-            if(browser != null) {
+        protected override void OnMouseUp(MouseEventArgs e)
+        {
+            if (browser != null)
+            {
                 SetMouseEvent(e);
                 CfxMouseButtonType t;
-                switch(e.Button) {
+                switch (e.Button)
+                {
                     case System.Windows.Forms.MouseButtons.Right:
                         t = CfxMouseButtonType.Left;
                         break;
@@ -300,66 +356,78 @@ namespace Windowless {
             }
         }
 
-		protected override void OnMouseWheel(MouseEventArgs e) {
-			if (browser != null)
-			{
-				SetMouseEvent(e);
-				browser.Host.SendMouseWheelEvent(mouseEventProxy, 0, e.Delta);
-			}
-		}
+        protected override void OnMouseWheel(MouseEventArgs e)
+        {
+            if (browser != null)
+            {
+                SetMouseEvent(e);
+                browser.Host.SendMouseWheelEvent(mouseEventProxy, 0, e.Delta);
+            }
+        }
 
         // key events
         // this is not complete
 
-        protected override void OnKeyPress(KeyPressEventArgs e) {
+        protected override void OnKeyPress(KeyPressEventArgs e)
+        {
 
-			//Console.WriteLine (e.KeyChar);
-            if(e.KeyChar == 7) {
+            //Console.WriteLine (e.KeyChar);
+            if (e.KeyChar == 7)
+            {
                 // ctrl+g - load google so we have a page with text input
                 browser.MainFrame.LoadUrl("https://www.baidu.com");
-            } else {
+            }
+            else
+            {
+                if (!e.Handled)
+                {
+                    var k = new CfxKeyEvent();
+                    k.WindowsKeyCode = e.KeyChar;
+                    k.Character = (short)e.KeyChar;
+                    k.Type = CfxKeyEventType.Char;
+                    k.UnmodifiedCharacter = (short)e.KeyChar;
+                    browser.Host.SendKeyEvent(k);
 
-				var j = new CfxKeyEvent();
-				j.WindowsKeyCode = e.KeyChar;
-				j.Character = (short)e.KeyChar;
-				j.Type = CfxKeyEventType.Keydown;
-
-				browser.Host.SendKeyEvent(j);
- 
-				var k = new CfxKeyEvent();
-				k.WindowsKeyCode = e.KeyChar;
-				k.Character = (short)e.KeyChar;
-				k.Type = CfxKeyEventType.Char;
-				k.UnmodifiedCharacter = (short)e.KeyChar;
-				browser.Host.SendKeyEvent(k);
-			 
-				base.OnKeyPress(e);
+                    e.Handled = true;
+                }
             }
         }
 
- 
-	protected override bool ProcessCmdKey(ref Message msg, Keys keyData) {
-			
-			switch (keyData)
-			{
-			case Keys.Down:
-			case Keys.Left:
-			case Keys.Right:
-			case Keys.Up:
-				{
-					var j = new CfxKeyEvent();
-					j.WindowsKeyCode = (int)keyData;
-					j.Type = CfxKeyEventType.RawKeydown;
-					browser.Host.SendKeyEvent(j);
-					return true;
-				}
-			default:
-				{
-					return base.ProcessCmdKey(ref msg, keyData);
-				}
-			}
-			
-		}
-	}
-    
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+
+            var j = new CfxKeyEvent();
+            j.WindowsKeyCode = e.KeyValue;
+            j.Character = (short)e.KeyValue;
+            j.Type = CfxKeyEventType.Keydown;
+
+            browser.Host.SendKeyEvent(j);
+        }
+
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+
+            switch (keyData)
+            {
+                case Keys.Down:
+                case Keys.Left:
+                case Keys.Right:
+                case Keys.Up:
+                    {
+                        var j = new CfxKeyEvent();
+                        j.WindowsKeyCode = (int)keyData;
+                        j.Type = CfxKeyEventType.RawKeydown;
+                        browser.Host.SendKeyEvent(j);
+                        return true;
+                    }
+                default:
+                    {
+                        return base.ProcessCmdKey(ref msg, keyData);
+                    }
+            }
+
+        }
+    }
+
 }
--- Tests/Windowless/ProgramWin.cs
+++ Tests/Windowless/ProgramWin.cs
@@ -9,7 +9,9 @@ using System.Windows.Forms;
 using Chromium;
 using System.Diagnostics;
 using System.IO;
+using System.Reflection;
 using System.Security;
+using System.Threading;
 
 namespace Windowless {
     static class ProgramWin {
@@ -18,6 +20,10 @@ namespace Windowless {
         private static string cachePath;
         private static string userPath;
         private static string exePath;
+
+        internal static CfxBrowserProcessHandler processHandler;
+        private static bool _mono;
+        internal static bool helperInitialized;
         [STAThread]
         static void Main() {
 
@@ -63,6 +69,9 @@ namespace Windowless {
 
             settings.ResourcesDirPath = System.IO.Path.Combine(exePath, "cef", "Resources");
             settings.LocalesDirPath = System.IO.Path.Combine(exePath, "cef", "Resources", "locales");
+
+             
+
             //settings.MultiThreadedMessageLoop = true;
             var app = new CfxApp();
             app.OnBeforeCommandLineProcessing += (s, e) => {
@@ -72,8 +81,8 @@ namespace Windowless {
                 //e.CommandLine.AppendSwitch("disable-gpu-vsync");
                 
             };
-
-            if(!CfxRuntime.Initialize(settings, app))
+            app.GetBrowserProcessHandler += App_GetBrowserProcessHandler;
+            if (!CfxRuntime.Initialize(settings, app))
                 Environment.Exit(-1);
 
 
@@ -88,17 +97,85 @@ namespace Windowless {
             Application.Idle += Application_Idle;
             Application.ApplicationExit += Application_ApplicationExit;
             Application.Run(f);
-            
+
+            CfxRuntime.Shutdown();
           
         }
+        static void App_GetBrowserProcessHandler(object sender, Chromium.Event.CfxGetBrowserProcessHandlerEventArgs e)
+        {
+            processHandler = new CfxBrowserProcessHandler();
+            processHandler.OnBeforeChildProcessLaunch += ProcessHandler_OnBeforeChildProcessLaunch;
+            e.SetReturnValue(processHandler);
+        }
 
+        static void ProcessHandler_OnBeforeChildProcessLaunch(object sender, Chromium.Event.CfxOnBeforeChildProcessLaunchEventArgs e)
+        {
+            var programPath = new Uri(Assembly.GetEntryAssembly().CodeBase).LocalPath;
+
+            if (!_mono)
+            {
+              //  SetJobObjectAsKillOnJobClose
+              
+            }
+            //			Console.WriteLine (programPath);
+
+            if (e.CommandLine.GetSwitchValue("type") == "gpu-process")
+            {
+                e.CommandLine.Program = programPath;
+            }
+            else
+            {
+                if (_mono)
+                {
+                    //var monoExeName = Assembly.GetEntryAssembly ().GetName ().Name+".exe";
+                    var currentP = System.Diagnostics.Process.GetCurrentProcess();
+
+
+                    //					Console.WriteLine ("Main CMDLINE : -- "+currentP.MainModule.FileName+"|| modulename: "+currentP.MainModule.ModuleName);
+                    //					Console.WriteLine ("---------------------------------------------");
+
+                    //System.Diagnostics.Process.GetCurrentProcess().StartInfo.FileName
+                    if (currentP.MainModule.FileName == currentP.MainModule.ModuleName)
+                    {
+
+                        Console.WriteLine("------native mkbundle mode--------------");
+                        e.CommandLine.Program = currentP.MainModule.FileName;
+
+                    }
+                    else
+                    {
+
+                        Console.WriteLine("------mono runtime mode--------------");
+                        //e.CommandLine.Program = Path.Combine (new System.IO.FileInfo (programPath).Directory.FullName, "cef", "Release64", "cefclient");
+                        e.CommandLine.Program = ProcessBundleClientHelper();
+                        //
+                        //					//e.CommandLine.AppendSwitch ("multi-threaded-message-loop");
+                        ////					e.CommandLine.AppendSwitch ("off-screen-rendering-enabled");
+                        //					e.CommandLine.AppendSwitch("renderer-cmd-prefix");
+
+
+                        //e.CommandLine.AppendSwitch("disable-text-input-focus-manager");
+                        //e.CommandLine.AppendSwitch("no-zygote");
+                        //e.CommandLine.AppendSwitchWithValue("type","utility");
+                        //e.CommandLine.AppendSwitch("use-views");
+
+                        //--no-zygote
+                    }
+                }
+            }
+
+            Console.WriteLine("child cmdline:" + e.CommandLine.CommandLineString);
+            Console.WriteLine("program: " + e.CommandLine.Program);
+        }
         private static void Application_ApplicationExit(object sender, EventArgs e)
         {
+            CfxRuntime.QuitMessageLoop();
             CfxRuntime.Shutdown();
         }
 
         static void Application_Idle(object sender, EventArgs e) {
             CfxRuntime.DoMessageLoopWork();
+            Thread.Yield();
         }
     }
 }
