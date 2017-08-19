66fcbd29613ee6f0267c7bd77b0605b0df9008e8
azraelrabbit <azraelrabbit@localhost>
2017-08-19 18:38:47 +0800

    add extension to help usage

---
+++ ChromiumFX.Extension/ResourceHelper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Chromium.WebBrowser;
+
+namespace ChromiumFX.Extension
+{
+    public static class ResourceHelper
+    {
+        public static Uri ResolveUri(string url)
+        {
+            var uri = new Uri(url, UriKind.RelativeOrAbsolute);
+
+            if (url.Contains("~/"))
+            {
+                var newUri = string.Empty;
+                var rootPath = uri.Scheme + "://" + uri.Host;
+                if (!string.IsNullOrEmpty(ChromiumStartup.VirtualPath))
+                {
+                    newUri = rootPath + "/" + ChromiumStartup.VirtualPath + "/";
+                }
+                var subPath = url.Substring(url.LastIndexOf("~/"));
+
+                newUri += subPath.Replace("~/", string.Empty);
+                uri = new Uri(newUri, UriKind.RelativeOrAbsolute);
+            }
+            if (!string.IsNullOrEmpty(ChromiumStartup.VirtualPath))
+            {
+                var tmpUrl = uri.ToString();
+                if (!tmpUrl.Contains(ChromiumStartup.VirtualPath))
+                {
+                    var newUri = string.Empty;
+                    var rootPath = uri.Scheme + "://" + uri.Host;
+                    if (!string.IsNullOrEmpty(ChromiumStartup.VirtualPath))
+                    {
+                        newUri = rootPath + "/" + ChromiumStartup.VirtualPath;
+                    }
+
+                    newUri += uri.PathAndQuery;
+                    uri = new Uri(newUri, UriKind.RelativeOrAbsolute);
+                }
+            }
+            return uri;
+        }
+    }
+}
